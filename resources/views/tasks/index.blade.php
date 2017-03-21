44f09810bbdd0d5ecb312f66d8c37858f452c006
yasutokuda <y.tokuda.1708@gmail.com>
2017-03-21 11:46:37 +0000

    create add kadai-tasklist

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク一覧</h1>
+
+    @if (count($tasks) > 0)
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{{ $task->id }} : {{ $task->content }} [{!! link_to_route('tasks.show', "詳細", ['id' => $task->id]) !!}]</li>
+            @endforeach
+        </ul>
+    @endif
+
+    {!! link_to_route('tasks.create', 'タスク作成') !!}
+
+@endsection
